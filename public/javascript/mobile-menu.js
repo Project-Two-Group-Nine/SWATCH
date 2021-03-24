918cf7939b49f66bc5febbff126b63aa5f2e723d
Deisel <74831846+Amelia-was@users.noreply.github.com>
2021-03-24 10:23:57 -0400

    second nav dropdown links

--- public/javascript/mobile-menu.js
+++ public/javascript/mobile-menu.js
@@ -10,6 +10,27 @@ const toggleMobileMenu = (e) => {
         menuToggle.style.visibility = 'visible';
         menu.style.display = 'none';
     }
-} 
+}
 
 document.querySelector('body').addEventListener('click', toggleMobileMenu)
+
+const navSecondary = document.querySelector('.nav-secondary');
+
+const showHoverMenu = (e) => {
+    if (e.target.classList.contains('hover-link')) {
+        console.log('hovered!');
+        const hoverMenu = e.target.nextElementSibling;
+        hoverMenu.style.display = 'block';
+    }
+}
+
+const hideHoverMenu = (e) => {
+    console.log('unhovered!');
+    const hoverMenus = document.getElementsByClassName('hover-menu');
+    for (let i = 0; i < hoverMenus.length; i++) {
+        hoverMenus[i].style.display = 'none';
+    }
+}
+
+navSecondary.addEventListener('mouseover', showHoverMenu);
+navSecondary.addEventListener('mouseleave', hideHoverMenu);
