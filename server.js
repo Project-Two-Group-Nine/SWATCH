1f35535261747ca6763bc474e5e13f4279b073ed
bluesatyr <shawnevans.music@gmail.com>
2021-03-25 19:51:10 -0400

    add pagination by query parameters (i.e. localhost:3001/?page=3)

--- server.js
+++ server.js
@@ -10,6 +10,9 @@ const PORT = process.env.PORT || 3001;
 
 const sequelize = require("./config/connection");
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const bodyParser = require('body-parser');
+const url = require('url');
+const querystring = require('querystring');
 
 const sess = {
   secret: 'Super secret secret',
@@ -35,7 +38,8 @@ app.set('view engine', 'handlebars');
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.use(require('./controllers/'));
 
